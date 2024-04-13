7fe9ac45024b59b54499c076d75ad7f2ba00e91f
juliaondrusova <125763375+juliaondrusova@users.noreply.github.com>
2024-04-13 21:36:14 +0200

    celkova zmena pristupu k serveru
    inspiracia projektom astack_modern - oproti predoslej verzii pridany multithreading, zmena citania dat z recv na makefile, krajsie vracenie odpovedi

--- yaml_server.py
+++ yaml_server.py
@@ -1,33 +1,106 @@
-#!/usr/bin/env python3
-
+import socket
+import multiprocessing
+import logging
 import os
 import yaml
-import socket
+
+class BadRequest(Exception):
+
+    pass
+
+class ConnectionClosed(Exception):
+
+    pass
+
+STATUS_OK=(100,'OK')
+STATUS_READ_ERROR=(201,'Read error')
+STATUS_FORMAT_ERROR=(202,'File format error')
+STATUS_NO_KEY=(200,'No such key')
+STATUS_UKNOWN_METHOD=(203,'Unkown method')
+STATUS_NO_FIELD = (204, 'No such field')
+STATUS_BAD_REQUEST=(300,'Bad request')
 
 
-def process_get_request(data):
+logging.basicConfig(level=logging.DEBUG)
+
+class Request:
+
+    def __init__(self,f):
+        
+        lines=[]
+        while True:
+            line=f.readline()
+            line=line.decode('utf-8')
+            if line=='':
+                if not lines:
+                    raise ConnectionClosed
+                else:
+                    logging.error('Klient zavrel spojenie priskoro')
+                    raise ConnectionClosed
+            if line=='\n':
+                break
+            line=line.rstrip()
+            logging.debug(f'Client sent {line}')
+            lines.append(line)
+        if not lines: # nic neposlal
+            raise BadRequest
+        self.method=lines[0]
+        self.content=lines[1:]
+        
+class Response:
+
+    def __init__(self,status,content=[]):
+
+        self.status=status
+        self.content=content
+
+    def send(self,f):
+        f.write(f'{self.status[0]} {self.status[1]}\n'.encode('utf-8'))
+        if self.status[0] == 100:
+            if self.content:
+                response_content = yaml.dump(self.content)
+                size = len(response_content.encode('utf-8'))
+                f.write(f"Content-length: {size}\n".encode('utf-8') + '\n'.encode('utf-8') + response_content.encode('utf-8'))
+        else:
+            f.write('\n'.encode('utf-8'))   
+        f.flush()
+
+    def __repr__(self):
+
+        return f'''Response(
+            {self.status[0]} {self.status[1]},
+            {self.content})'''
+        
+
+def method_GET(request,stack):
+
+    if not request.content:
+        return Response(STATUS_BAD_REQUEST)
+    
     filename = ""
     fieldname = ""
-    for header in data:
+
+    for header in request.content:
         key_value = header.split(":", 1)
         if len(key_value) == 2:
             key = key_value[0].strip()
             value = key_value[1].strip()
             if key == "Key":
                 if any(c in value for c in (' ', ':', '/')):
-                    return "300 Bad request\n\n"
+                    return Response(STATUS_BAD_REQUEST)
                 else:
                     filename = value + ".yaml"
             elif key == "Field":
                 if ' ' in value:
-                    return "300 Bad request\n\n"
+                    return Response(STATUS_BAD_REQUEST)
                 else:
                     fieldname = value
             else:
-                return "300 Bad request\n\n"
+                 return Response(STATUS_BAD_REQUEST)
         else:
-            return "300 Bad request\n\n"
+             return Response(STATUS_BAD_REQUEST)
     if filename:
+
         if os.path.exists('data') and os.path.isdir('data'):
             file_found = filename in os.listdir('data')
             if file_found:
@@ -36,168 +109,121 @@ def process_get_request(data):
                     with open(file_path, mode='r') as f:
                         dict = yaml.safe_load(f)
                         if fieldname in dict:
-                            yaml_repr = yaml.dump(dict[fieldname])
-                            size = len(yaml_repr.encode('utf-8'))
-                            response = f"100 OK\nContent-length: {size}\n\n{yaml_repr}"
-                            return response
+                            return Response(STATUS_OK,dict[fieldname])        
                         else:
-                            return "204 No Such field\n\n"
+                            return Response(STATUS_NO_FIELD)
                 except OSError:
-                    return "201 Read error\n"
+                    return Response(STATUS_READ_ERROR)
                 except yaml.error.YAMLError:
-                    return "202 File format error\n\n"
+                    return Response(STATUS_FORMAT_ERROR)
             else:
-                return "200 No such key\n\n"
+                return Response(STATUS_NO_KEY)
         else:
-            return "300 Bad request\n\n"
+            return Response(STATUS_BAD_REQUEST)
     else:
-        return "300 Bad request\n\n"
+        return Response(STATUS_BAD_REQUEST)
 
 
-def process_keys_request(data):
+
+
+
+def method_KEYS(request,stack):
+
     if os.path.exists('data') and os.path.isdir('data'):
         yaml_files = [f for f in os.listdir('data') if f.endswith('.yaml')]
         yaml_files_without_extension = [os.path.splitext(f)[0] for f in yaml_files]
         nazov = yaml.dump(yaml_files_without_extension)
         size = len(nazov.encode('utf-8'))
-        response = f"100 OK\nContent-length: {size}\n\n{nazov}"
-        return response
+        return Response(STATUS_OK, yaml_files_without_extension)
     else:
-        return "201 Read error\n\n"
+        return Response(STATUS_READ_ERROR)
+    
+
+def method_FIELD(request,stack):
+
+    if not request.content:
+        return Response(STATUS_BAD_REQUEST)
+
+    key_value = request.content[0].split(":", 1)
+    if len(key_value) == 2:
+        key = key_value[0].strip()
+        value = key_value[1].strip()
+    else:
+        return Response(STATUS_BAD_REQUEST)
+
+    if key != "Key":
+        return Response(STATUS_BAD_REQUEST)
+    
+    if any(c in value for c in (' ', ':', '/')):
+        return Response(STATUS_BAD_REQUEST)
+    
+    filename = value + ".yaml"
+
+    if os.path.exists('data') and os.path.isdir('data'):
+        file_path = os.path.join('data', filename)
+        try:
+            with open(file_path) as f:
+                data_dict = yaml.safe_load(f)
+                return_value = list(data_dict)
+                return Response(STATUS_OK, return_value)
+        except FileNotFoundError:
+            return Response(STATUS_NO_KEY)
+        except OSError:
+            return Response(STATUS_READ_ERROR)
+        except yaml.error.YAMLError:
+            return Response(STATUS_FORMAT_ERROR)
+        except (KeyError, TypeError):
+            return Response(STATUS_NO_FIELD)
+    else:
+        return Response(STATUS_NO_KEY)
 
 
-def process_fields_request(data):
-    filename = ""
-    fieldname = ""
 
-    for header in data:
-        print("Header " + header)
-        key_value = header.split(":", 1)
-        print(key_value)
-        if len(key_value) == 2:
-            key = key_value[0].strip()
-            value = key_value[1].strip()
-            print("Key "+key+"="+value)
-            if key == "Key":
-                if any(c in value for c in (' ', ':', '/')):
-                    return "300 Bad request\n\n"
-                else:
-                    filename = value + ".yaml"
-            else:
-                return "300 Bad request\n\n"
-        else:
-            return "300 Bad request\n\n"
+METHODS={
+    'GET':method_GET,
+    'KEYS':method_KEYS,
+    'FIELDS':method_FIELD
+}
 
-    if filename:
-        file_found = filename in os.listdir('data')
-        if file_found:
-            file_path = os.path.join('data', filename)
+
+def handle_client(client_socket,addr):
+
+    stack=[]
+
+    logging.info(f'handle_client {addr} start')
+    f=client_socket.makefile('rwb')
+    try:
+        while True:
             try:
-                with open(file_path, mode='r') as f:
-                    dict = yaml.safe_load(f)
-                    keys = list(dict)
-                    string = yaml.safe_dump(keys)
-                    size = len(string.encode('utf-8'))
-                    response = f"100 OK\nContent-length: {size}\n\n{string}"
-                    return response
-            except OSError:
-                return "201 Read error\n\n"
-            except yaml.error.YAMLError:
-                return "202 File format error\n\n"
-        else:
-            return "200 No such key\n\n"
-    else:
-        return "200 No such key\n\n"
-
-def process_request(data):
-    lines = data.splitlines()
-    if lines:
-        method = lines[0]
-        lines = data.splitlines()[1:]
-        if method == "GET":
-            return process_get_request(lines)
-        elif method == "KEYS":
-            return process_keys_request(lines)
-        elif method == "FIELDS":
-            return process_fields_request(lines)
-        else:
-            return "203 Unkown method\n\n"
-
-    else:
-        return "203 Unkown method\n\n"
-
-def server_program():
-    host = "localhost"
-    port = 9999  # initiate port no above 1024
-    request = ""
-    server_socket = socket.socket()  # get instance
-    # look closely. The bind() function takes tuple as argument
-    server_socket.bind((host, port))  # bind host address and port together
-
-    # configure how many client the server can listen simultaneously
-    server_socket.listen(2)
-    conn, address = server_socket.accept()  # accept new connection
-    print("Connection from: " + str(address))
-    
-    step = 0
-    request = ""
-    while True:
-        # receive data stream. it won't accept data packet greater than 1024 bytes
-        lines = conn.recv(1024).decode("utf-8")
-        print("Lines: "+lines)
-        if not lines:
-            print("koniec spojenia")
-            break
-        lines = lines.splitlines()
-        if len(lines)==0:
-            response = process_request(request)
-            conn.sendall(response.encode("utf-8"))  # send data to the client
-            step = 0
-            request = ""
-            continue
-    
-        for data in lines:
-            print("Prijate data: "+data)
-        
-            #kontrolujeme metodu
-            if step == 0:
-                if data not in ["GET", "FIELDS", "KEYS"]:
-                    response = "203 Unkown method\n\n"
-                    conn.sendall(response.encode("utf-8"))
-                    step = 0
-                    print("Bad method")
-                    request = ""
-                    break
-            #kontrolujeme hlavicku
+                req=Request(f)
+                logging.info(f'Request: {req.method} {req.content}')
+            except BadRequest:
+                logging.info(f'Bad request {addr}')
+                break 
+            except ConnectionClosed:
+                logging.info(f'Connection closed {addr}')
+                break
+            if req.method in METHODS:
+                response=METHODS[req.method](req,stack)
             else:
-                if data == "":
-                    print("Request:" + request)
-                    response = process_request(request)
-                    conn.sendall(response.encode("utf-8"))  # send data to the client
-                    step = 0
-                    request = ""
-                    break
-                else:
-                    header = data.split(":", 1)
-                    if len(header) == 2:
-                        key = header[0].strip()
-                        if key not in ["Key", "Field"]:
-                            response = "300 Bad request\n\n"
-                            conn.sendall(response.encode("utf-8"))
-                            step = 0
-                            break
-                    else:
-                        response = "300 Bad request\n\n"
-                        conn.sendall(response.encode("utf-8"))
-                        step = 0
-                        break   
-            request= request + data + "\n"            
-            step=step+1
+                response=Response(STATUS_UKNOWN_METHOD)
+            logging.info(f'{response}')
+            response.send(f)
+           
+        logging.info(f'handle_client {addr} stop')
+    except KeyboardInterrupt:
+        client_socket.close()
+    #client_socket.close()
 
-    conn.close()  # close the connection
+s=socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+s.setsockopt(socket.SOL_SOCKET,socket.SO_REUSEADDR,1)
+s.bind(('',9999))
+s.listen(5)
 
+while True:
 
-
-
-if __name__ == '__main__':
-    server_program()
+    cs,addr=s.accept()
+    process=multiprocessing.Process(target=handle_client,args=(cs,addr))
+    process.daemon=True
+    process.start()
+    cs.close()
